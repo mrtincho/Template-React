f7ea9b6b4d0f3f411d5361a7a61c91b23b6187ba
m_cesarini <m_cesarini@hotmail.com>
2018-03-25 18:07:09 -0400

    Template Terminado

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'; //Importo React de react y tambien Component (asi evito React.Component y uso Component solo)
 import Reactdom, { render } from 'react-dom'; //Importo render de react-dom para mostrar datos
+import App from './components/app.jsx';
 
 class Prueba extends React.Component {
   render() {
     return (
-      <h1>Hola Mundo</h1>
+      <h1>Titulo del Sitio</h1>
     ) 
   }
 }
 
 Reactdom.render(
-  <Prueba/>
+  <App/>
   , document.getElementById('app')
+);
+Reactdom.render(
+  <Prueba/>
+  , document.getElementById('titulo')
 );
